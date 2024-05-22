6cce7053f8e6f1d4f284c9287c69727d5a655d1e
Angel Velasquez <pcisavel@upc.edu.pe>
2024-05-22 16:34:02 -0500

    feat(profiles): added command service implementation.

--- src/main/java/com/acme/center/platform/profiles/infrastructure/persistence/jpa/repositories/ProfileRepository.java
+++ src/main/java/com/acme/center/platform/profiles/infrastructure/persistence/jpa/repositories/ProfileRepository.java
@@ -10,4 +10,5 @@ import java.util.Optional;
 @Repository
 public interface ProfileRepository extends JpaRepository<Profile, Long> {
     Optional<Profile> findByEmail(EmailAddress emailAddress);
+    boolean existsByEmail(EmailAddress emailAddress);
 }
