66c5335497541b5f7d16339c825f8462209347df
Angel Velasquez <pcisavel@upc.edu.pe>
2024-06-05 15:44:12 -0500

    feat(iam): added role entity.

---
+++ src/main/java/com/acme/center/platform/iam/domain/model/entities/Role.java
@@ -0,0 +1,44 @@
+package com.acme.center.platform.iam.domain.model.entities;
+
+import com.acme.center.platform.iam.domain.model.valueobjects.Roles;
+import jakarta.persistence.*;
+import lombok.Getter;
+
+import java.util.List;
+
+@Entity
+public class Role {
+    @Getter
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Getter
+    @Column(nullable = false, unique = true, length = 20)
+    private Roles name;
+
+    public Role() {}
+
+    public Role(Roles name) {
+        this.name = name;
+    }
+
+    public String getStringName() {
+        return name.name();
+    }
+
+    public static Role getDefaultRole() {
+        return new Role(Roles.ROLE_USER);
+    }
+
+    public static Role toRoleFromName(String name) {
+        return new Role(Roles.valueOf(name));
+    }
+
+    public static List<Role> validateRoleSet(List<Role> roles) {
+        if (roles == null || roles.isEmpty()) {
+            return List.of(getDefaultRole());
+        }
+        return roles;
+    }
+}
