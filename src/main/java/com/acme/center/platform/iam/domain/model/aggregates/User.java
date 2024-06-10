6c16b01f422da1f5283b711810179ebcd4827aec
Angel Velasquez <pcisavel@upc.edu.pe>
2024-06-10 17:16:29 -0500

    feat(iam): added models for spring framework security support.

--- src/main/java/com/acme/center/platform/iam/domain/model/aggregates/User.java
+++ src/main/java/com/acme/center/platform/iam/domain/model/aggregates/User.java
@@ -24,6 +24,7 @@ public class User extends AuditableAbstractAggregateRoot<User> {
     @Size(max = 120)
     private String password;
 
+    @Getter
     @ManyToMany
     @JoinTable(name = "user_roles",
     joinColumns = @JoinColumn(name = "user_id"),
---
+++ src/main/java/com/acme/center/platform/iam/infrastructure/authorization/sfs/model/UserDetailsImpl.java
@@ -0,0 +1,46 @@
+package com.acme.center.platform.iam.infrastructure.authorization.sfs.model;
+
+import com.acme.center.platform.iam.domain.model.aggregates.User;
+import com.acme.center.platform.iam.domain.model.entities.Role;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Getter
+@EqualsAndHashCode
+public class UserDetailsImpl implements UserDetails {
+    private final String username;
+    @JsonIgnore
+    private final String password;
+    private final boolean accountNonExpired;
+    private final boolean accountNonLocked;
+    private final boolean credentialsNonExpired;
+    private final boolean enabled;
+    private final Collection<? extends GrantedAuthority> authorities;
+
+    public UserDetailsImpl(String username, String password, Collection<? extends GrantedAuthority> authorities) {
+        this.username = username;
+        this.password = password;
+        this.authorities = authorities;
+        this.accountNonExpired = true;
+        this.accountNonLocked = true;
+        this.credentialsNonExpired = true;
+        this.enabled = true;
+    }
+
+    public static UserDetailsImpl build(User user) {
+        var authorities = user.getRoles().stream()
+                .map(Role::getStringName)
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+        return new UserDetailsImpl(user.getUsername(), user.getPassword(), authorities);
+    }
+
+}
